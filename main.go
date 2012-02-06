6241a8b1a76fa543a25a7a8e57decc142b228c44
Andreas Krennmair <ak@synflood.at>
2012-02-06 14:36:16 +0100

    log all requests.

--- main.go
+++ main.go
@@ -19,27 +19,32 @@ func main() {
 
 	// the / prefix is special... it handles all the request that didn't match the other prefixes
 	// that's why we create the custom StartPageHandler.
-	servemux.Handle("/", &StartPageHandler{Other: http.FileServer(http.Dir("htdocs"))})
-	servemux.Handle("/progress/", &ProgressHandler{Persistence: pm})
-	servemux.Handle("/upload/", &PostUploadHandler{Persistence: pm})
-	servemux.Handle("/show/", &ShowHandler{Persistence: pm})
-	servemux.Handle("/savedesc/", &SaveDescHandler{Persistence: pm})
-	servemux.Handle("/files/", &DeliverFileHandler{Persistence: pm})
-	servemux.Handle("/requpid", &RequestIDHandler{})
+	servemux.Handle("/", NewLoggingHandler(&StartPageHandler{Other: http.FileServer(http.Dir("htdocs"))}))
+	servemux.Handle("/progress/", NewLoggingHandler(&ProgressHandler{Persistence: pm}))
+	servemux.Handle("/upload/", NewLoggingHandler(&PostUploadHandler{Persistence: pm}))
+	servemux.Handle("/show/", NewLoggingHandler(&ShowHandler{Persistence: pm}))
+	servemux.Handle("/savedesc/", NewLoggingHandler(&SaveDescHandler{Persistence: pm}))
+	servemux.Handle("/files/", NewLoggingHandler(&DeliverFileHandler{Persistence: pm}))
+	servemux.Handle("/requpid", NewLoggingHandler(&RequestIDHandler{}))
 
 	// create HTTP server and run it on port 8000.
 	httpsrv := &http.Server{Handler: servemux, Addr: "0.0.0.0:8000"}
 	httpsrv.ListenAndServe()
 }
 
-// this function creates wrapper functions for http.ServeMux handler functions
-// that log all requests that are directed to the handler function specified
-// as argument.
-func logReq(f func(rw http.ResponseWriter, r *http.Request)) func(rw http.ResponseWriter, r *http.Request) {
-	return func(rw http.ResponseWriter, r *http.Request) {
-		LogHTTPRequest(r)
-		f(rw, r)
-	}
+type LoggingHandler struct {
+	handler http.Handler
+}
+
+// this function creates a wrapper handler that logs all HTTP requests for the specified
+// http.Handler
+func NewLoggingHandler(h http.Handler) http.Handler {
+	return &LoggingHandler{handler: h}
+}
+
+func (h *LoggingHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	LogHTTPRequest(r)
+	h.handler.ServeHTTP(w, r)
 }
 
 func MakeResponseNonCachable(rw *http.ResponseWriter) {
