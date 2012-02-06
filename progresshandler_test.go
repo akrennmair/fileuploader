5125212088f2cf74643d357c4e1c186efe4bc014
Andreas Krennmair <ak@synflood.at>
2012-02-06 12:16:28 +0100

    oops, forgot to check in changes to this file.

--- progresshandler_test.go
+++ progresshandler_test.go
@@ -2,17 +2,14 @@ package main
 
 import (
 	"testing"
-	"errors"
-	"bytes"
 	"net/http"
 	"fmt"
 	"strings"
-	"io"
 )
 
 
 func TestProgressHandler(t *testing.T) {
-	pm := &MockPersistenceManager{}
+	pm := NewMockPersistenceManager()
 
 	ph := &ProgressHandler{Persistence: pm}
 
@@ -54,75 +51,5 @@ func TestProgressHandler(t *testing.T) {
 	if !strings.Contains(resp.Buffer.String(),"invalid Upload ID") {
 		t.Errorf("ProgressHandler didn't detect an invalid upload ID as such")
 	}
-
 }
 
-type MockResponseWriter struct {
-	StatusCode int
-	Buffer      *bytes.Buffer
-	header      http.Header
-}
-
-func NewMockResponseWriter() *MockResponseWriter {
-	return &MockResponseWriter { Buffer: new(bytes.Buffer), header: make(http.Header) };
-}
-
-func (w *MockResponseWriter) Header() http.Header {
-	return w.header
-}
-
-func (w *MockResponseWriter) WriteHeader(c int) {
-	w.StatusCode = c
-}
-
-func (w *MockResponseWriter) Write(b []byte) (int, error) {
-	if w.StatusCode == 0 {
-		w.StatusCode = http.StatusOK
-	}
-	return w.Buffer.Write(b)
-}
-
-type MockPersistenceManager struct {
-	Progress int
-}
-
-func (h *MockPersistenceManager) WriteUploadProgress(upload_id string, percent int) error {
-	h.Progress = percent
-	return nil
-}
-
-func (h *MockPersistenceManager) GetUploadProgress(upload_id string) (percent int, err error) {
-	return h.Progress, nil
-}
-
-func (h *MockPersistenceManager) OpenUpload(upload_id string) (io.ReadCloser, error) {
-	return nil, errors.New("mock persistence manager")
-}
-
-func (h *MockPersistenceManager) OpenUploadWritable(upload_id string) (io.WriteCloser, error) {
-	return nil, errors.New("mock persistence manager")
-}
-
-func (h *MockPersistenceManager) UploadExists(upload_id string) bool {
-	return true
-}
-
-func (h *MockPersistenceManager) SaveUploadText(upload_id, text string) error {
-	return nil
-}
-
-func (h *MockPersistenceManager) GetUploadText(upload_id string) (string, error) { 
-	return "", nil
-}
-
-func (h *MockPersistenceManager) SaveUploadFilename(upload_id, filename string) error { 
-	return nil
-}
-
-func (h *MockPersistenceManager) GetUploadFilename(upload_id string) (string, error) { 
-	return "", nil
-}
-
-func (h *MockPersistenceManager) GetUploadSize(upload_id string) (size int64, err error) { 
-	return 23, nil
-}
